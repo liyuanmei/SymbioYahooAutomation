a62b17034dd91f6f9ca5c2cbe205fdb888b2f707
zhidongzhang <zhidong.zhang@symbio.com>
2014-06-17 11:13:35 +0800

    update

--- UIAutomation/UIAutomation/Function/Welcome.js
+++ UIAutomation/UIAutomation/Function/Welcome.js
@@ -1,31 +1,32 @@
-test("Welcomen page test script, need run this script before all other scripts", function () {
-	$.delay(12);
+test("Welcomen page test script, need run this script before all other scripts" , function  () {
+	$.delay(10);
+
 	var logPage = app.mainWindow().buttons()[0].name();
 	$.delay(sleep);
-	if(logPage == "登入"){
 
-	//Verify elements on Welcome screen show correct
-	Assert.verifyWelcomeScreen();
-	$.delay(sleep);
+	if (logPage == "登入") {
 
-	//Tap "略過，前往下一步" button.
-	Action.tapNextButtonOnWelcomeScreen();
-    }
-    else{
-    	Assert.checkStartUsing();
-    	$.delay(sleep);
+		//Verify elements on Welcome screen show correct
+		Assert.verifyWelcomeScreen();
+		$.delay(sleep);
 
-    	//tap "開始使用"
-        Action.tapCreatePersonalExperience();
-    }
+		//Tap &quot;略過，前往下一步&quot; button.
+		Action.tapNextButtonOnWelcomeScreen();
+	}
+	else{
+		Assert.checkStartUsing();
+		$.delay(sleep);
 
-    //Verify elements on Personal category show correct.
+		//tap "開始使用";
+		Action.tapCreatePersonalExperience();
+	}
+	//Verify elements on Personal category show correct.
     Assert.personalCategoryScreen();
 
-    //Tap "建立個人化的購物體驗"
+    //Tap &quot;建立個人化的購物體驗&quot;
     Action.tapCreatePersonalExperience();
     $.delay(15);
 
-    //Verify screen successful navigate to "最新動態" screen.
+    //Verify screen successful navigate to &quot;最新動態&quot; screen.
     Assert.discoveryScreenShowCorrect();
 });
