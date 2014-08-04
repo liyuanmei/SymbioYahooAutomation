8c24a5b8f0ad0b519966f6daf25d1960cee008c4
zhidongzhang <zhidong.zhang@symbio.com>
2014-08-04 20:20:01 +0800

    debug

--- UIAutomation/UIAutomation/Function/StorePage.js
+++ UIAutomation/UIAutomation/Function/StorePage.js
@@ -62,25 +62,42 @@ test("[1959904] Verify user can check purchasing info from store page" , functio
     Action.chooseTheSizeOnShoppingCart();
     Action.tapConfirmOnShoppingCart();
     Action.tapButtonOnTabBar(3);
-    
-    Action.tapShoppingCartlist(0);
-    $.delay(25);
+    var version = target.systemVersion();
+    version = version.substring(0, version.lastIndexOf("."));
+    if(version == "6.1") {
+        $.delay(10);
+    }
+    else{
+        $.delay(5);
+    }
+    try{
+        $.delay(10);
+        Action.tapShoppingCartlist(0);
+        $.delay(10);
 
-    Assert.checkbutButtonShoppingCart();
-    $.delay(3);
+        Action.tapButtonOnTabBar(3);
+
+        Action.tapShoppingCartlist(0);
+        $.delay(30);
+
+        Assert.checkbutButtonShoppingCart();
+        $.delay(3);
     
-    Action.tapStoreLogoWhenStorePage();
-    Action.tapShoppingInformationPage();
-    Assert.ShoppingInformationPage();
-    
-    Action.goBack();
+        Action.tapStoreLogoWhenStorePage();
+        Action.tapShoppingInformationPage();
+        Assert.ShoppingInformationPage();
+        Action.goBack();
+    }
+    catch(err){
+        $.delay(10);
+        Action.tapButtonOnTabBar(3);
+    }
     Action.tapButtonOnTabBar(3);
     $.delay(sleep);
 
     Action.tapDeleteOnShoppingCart();
     $.delay(sleep);
 
-    Action.tapButtonOnTabBar(3);
     Action.tapButtonOnTabBar(2);
     Action.tapButtonOnTabBar(2);
     Action.tapButtonOnTabBar(2);
@@ -147,11 +164,12 @@ test("[1959898] Not log in, click on the add collection list icon ", function ()
         Action.doRefreshFavoriteStorePage();
         $.delay(10);
     }
+
     Action.tapButtonOnTabBar(0);
-    $.delay(sleep);
+    $.delay(5);
 
     Action.tapAddAccountOnLogin(varTestsSignInAccountMobilestoresymbio2 , varTestsSignInPasswordMobilestoresymbio2);
-    $.delay(5);
+    $.delay(10);
 
     Action.tapButtonOnTabBar(0);
 });
@@ -162,45 +180,51 @@ test("[1959925] Verify user can search funtion in store page ", function () {
     Action.tapButtonOnTabBar(2);
 
     Action.tapItemOnCategoryScreenWhenOptions(0);
+    $.delay(15);
 
     //go to 漢神百貨品牌服飾
     Action.tapChoosePreductCategoryWhenOptions(0,1);
-    $.delay(5);
+    $.delay(15);
 
     //go to 漢神百貨品牌
     Action.tapChoosePreductCategoryWhenOptions(0,3);
-    $.delay(10);
+    $.delay(15);
 
     Action.tapCommodityPictureOnSearchResultsPage();
-    $.delay(10);
+    $.delay(15);
 
     obj.scrollDowns(2);
     $.delay(sleep);
+    try{
+        var version = target.systemVersion();
+        version = version.substring(0, version.lastIndexOf("."));
+        if(version == "6.1") {
+           Action.tapChooseOnItemPage(3);
+        }
+        else{
+           Action.tapChooseOnItemPage(varTestsItemPageLinksLookGoods);
+        }
+        $.delay(5);
 
-    var version = target.systemVersion();
-    version = version.substring(0, version.lastIndexOf("."));
-    if(version == "6.1") {
-        Action.tapChooseOnItemPage(3);
-    }
-    else{
-        Action.tapChooseOnItemPage(varTestsItemPageLinksLookGoods);
-    }
-    $.delay(5);
+        Action.tapSearchIconOnNavBarWhenSRP();
+        $.delay(sleep);
 
-    Action.tapSearchIconOnNavBarWhenSRP();
-    $.delay(sleep);
-
-    Action.searchBarInputChinese(varTestsSearchBoxInputDataTokyo);
-    Action.tapKeyboardSearch();
-    $.delay(10);
+        Action.searchBarInputChinese(varTestsSearchBoxInputDataTokyo);
+        Action.tapKeyboardSearch();
+        $.delay(10);
  
-    Action.tapCommodityNameOnSearchResultsPage();
-    $.delay(15);
+        Action.tapCommodityNameOnSearchResultsPage();
+        $.delay(15);
 
-    obj.scrollDowns(1);
-    $.delay(10);
+        obj.scrollDowns(1);
+        $.delay(10);
 
-    Assert.itemPageShowCorrect();
+        Assert.itemPageShowCorrect();
+    }
+    catch(err){
+        $.delay(10);
+        Action.tapButtonOnTabBar(2);
+    }
 
     Action.tapButtonOnTabBar(2);
     Action.tapButtonOnTabBar(0);
@@ -214,7 +238,7 @@ test("[1959906] Verify store promotion item's display", function () {
     $.delay(sleep);
 
     Action.tapItemOnCategoryScreenWhenItemPage(0);
-    $.delay(sleep);
+    $.delay(5);
 
     Action.goCommodityTab();
     $.delay(10);
@@ -229,15 +253,22 @@ test("[1959906] Verify store promotion item's display", function () {
     $.delay(5);
 
     Action.tapCancelOnShoppingCart(); 
-    Action.tapChooseOnItemPage(varTestsPageNameOnSalesPromotion);
-    $.delay(5);
 
-    Assert.checkSearchPage(varTestsPageNameOnSalesPromotion);
-    Assert.checkSalesPromotionActivity();
-    Action.tapChoosePreductCategoryWhenOptions(0,0);
-    $.delay(15);
+    try{
+        Action.tapChooseOnItemPage(varTestsPageNameOnSalesPromotion);
+        $.delay(5);
 
-    Assert.checkTextShowCorrectly(2,varTestsCheckTextShowCorrectlyOnSalesPromotion)
+        Assert.checkSearchPage(varTestsPageNameOnSalesPromotion);
+        Assert.checkSalesPromotionActivity();
+        Action.tapChoosePreductCategoryWhenOptions(0,0);
+        $.delay(30);
+
+        Assert.checkTextShowCorrectly(2,varTestsCheckTextShowCorrectlyOnSalesPromotion);
+    }
+    catch(err){
+        $.delay(5);
+        Action.tapButtonOnTabBar(2);
+    }
 
     Action.tapButtonOnTabBar(2);
     Action.tapButtonOnTabBar(0);
@@ -290,12 +321,17 @@ test("[1959924] Verify store promotion item's display", function () {
 
     Action.tapCancelOnShoppingCart();
     $.delay(sleep);
-     
-    Action.tapChooseOnItemPage(varTestsPageNameOnSalesPromotion);
-    $.delay(15);
+    try{
+        Action.tapChooseOnItemPage(varTestsPageNameOnSalesPromotion);
+        $.delay(15);
 
-    Assert.checkSearchPage(varTestsPageNameOnSalesPromotion);
-    Assert.checkSalesPromotionActivity();
+        Assert.checkSearchPage(varTestsPageNameOnSalesPromotion);
+        Assert.checkSalesPromotionActivity();
+    }
+    catch(err){
+        $.delay(5);
+        Action.tapButtonOnTabBar(2);
+    }
 
     Action.tapButtonOnTabBar(2);
     Action.tapButtonOnTabBar(2);
@@ -311,13 +347,13 @@ test("[1959887] Verify '購買人次' display", function () {
     Action.tapSearchIconOnNavBar();
     Action.searchBarInputChinese(varTestsSearchBoxInputDataStore1);
     Action.tapKeyboardSearch();
-    $.delay(sleep);
+    $.delay(5);
 
     Action.tapStoreTab();
     $.delay(10);
 
     Action.tapSearchResultOfStore();
-    $.delay(5);
+    $.delay(15);
 
     var buyNum = app.mainWindow().collectionViews()[0].cells()[2].staticTexts()[1];
     Assert.elementsShouldContainText(buyNum,varTestsElementsShouldContainTextBuy);
